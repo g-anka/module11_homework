18a41ae9bceec9c4a08019ce62f88c9302b52798
g-anka <76132583+g-anka@users.noreply.github.com>
2021-04-27 23:06:39 +0300

    Update Задание_5.js

--- Задание_5.js
+++ Задание_5.js
@@ -1,8 +1,6 @@
 // Напишите функцию, которая принимает два натуральных числа x и n и возвращает x в степени n. Иначе говоря, умножает x на себя n раз и возвращает результат. Используйте Arrow Function синтаксис.
   
 
-const funcNumbers = (x, n) => {
-  console.log(x**n)
-}
+const funcNumbers = (x, n) => console.log(x**n);
 
 funcNumbers(2, 4);
